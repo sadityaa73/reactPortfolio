08a46b4c1197b043774823522cbecf46d4ab88e5
aditya singh <sadityaa73@gmail.com>
2024-04-10 15:47:50 +0530

    new changes

--- src/components/contentComponent/content.js
+++ src/components/contentComponent/content.js
@@ -1,5 +1,14 @@
 import { useState } from "react";
 import "./content.css";
+import vue from "./assets/vue.png";
+import react from "./assets/react.png";
+import js from "./assets/js.png";
+import html from "./assets/html.png";
+import css from "./assets/css.png";
+import wordpress from "./assets/wordpress.png";
+import node from "./assets/nodejs.png";
+import arrow from "./assets/arrowDown.png";
+
 export default function Content() {
     const [scroll, setScroll] = useState(0);
     const onScroll = () => {
@@ -11,14 +20,28 @@ export default function Content() {
         setScroll(ScrollPercent);
         document.getElementById("myBar").style.width=scroll+"%";
   };
-  window.addEventListener("scroll",onScroll);
   return (
     <div className="contentContainer" id="scrollContainer" onScroll={onScroll}>
         <div className="pageScrollerLoader">
           <div className="progressLoader" id="myBar" style={{width:`${scroll}%`}}></div>
         </div>
         <section className="introSec" id="sec01">
-          <div className="intro"></div>
+          <div className="back"></div>
+          <div className="contact">
+            <img src="" alt="" className="contact-img"></img>
+          </div>
+          <img src={vue} alt="vue.js" className="skill1"/>
+          <img src={react} alt="react.js" className="skill2"/>
+          <img src={js} alt="javascript" className="skill3"/>
+          <img src={html} alt="html" className="skill4"/>
+          <img src={css} alt="css" className="skill5"/>
+          <img src={wordpress} alt="wordpress" className="skill6"/>
+          <img src={node} alt="node.js" className="skill7"/>
+          <div className="sec1Content">
+            <div className="yellow"></div>
+            <div className="sec1_content"></div>
+          </div>
+          <a className="btn-readMore"><h6>readMore</h6><span><img src={arrow} alt="downArrow"/></span></a>
         </section>
         <section className="introSec" id="sec01">
           <div className="intro"></div>
